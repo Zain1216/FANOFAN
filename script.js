b2863961b806f7a049acb87e4f94cc85212ceab9
Zainul Abideen <121451360+Zain1216@users.noreply.github.com>
2024-10-18 14:57:20 -0700

    update index.html

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(()=>{
     $.get('footer.html',(data)=>{
         $('#footer').html(data)
     })
+   
 })
 
 // --------------------headerbanner----------------------
@@ -75,3 +76,109 @@ function showSlider(type) {
 // Start the initial animation 
 resetTimeAnimation()
     
+// ------------card-----------
+$(document).ready(function () {
+    const cardsData = [
+        {
+            title: "Ceiling Fan",
+            description: "Unmatched comfort",
+            imageUrl: "https://i.pinimg.com/474x/b2/c1/e0/b2c1e0c703a9dc3dbf6fea568aaa68d8.jpg",
+            colors: ["White", "Black", "Wood finishes", "Brushed Nickel"],
+            sizes: ["S(24-30 in)", "M(36-42 in)", "L(52-60 in)"],
+            features: ["Lighting", "Remote control", "Energy efficiency"],
+            weight: "10 to 25 lb",
+            cardLink: "  "
+        },
+        {
+            title: "Table Fan",
+            description: "Versatile Cooling",
+            imageUrl: "https://i.pinimg.com/474x/86/a1/bd/86a1bd35e26f0343a5b01597a7614035.jpg",
+            colors: ["White", "Blue", "Black"],
+            sizes: ["Small", "Medium"],
+            features: ["Oscillating", "Quiet operation"],
+            weight: "5 to 10 lb",
+            cardLink: "  "
+        },
+        {
+            title: "Stand Fan",
+            description: "Flexible Positioning",
+            imageUrl: "https://i.pinimg.com/474x/69/13/5b/69135be66bf711a18d24172e9bf67709.jpg",
+            colors: ["White", "Black", "Red"],
+            sizes: ["Adjustable<br>height"],
+            features: ["Remote control", "3 speed settings"],
+            weight: "7 to 12 lb",
+            cardLink: "  "
+        },
+        {
+            title: "Exhaust Fan",
+            description: "Pure Ventilation",
+            imageUrl: "https://i.pinimg.com/474x/09/0c/18/090c18991a24003418312fa68c464c54.jpg",
+            colors: ["White", "Black", "Stainless Steel"],
+            sizes: ["Standard", "Large"],
+            features: ["High airflow", "Energy efficient"],
+            weight: "8 to 15 lb",
+            cardLink: "  "
+        }
+    ];
+
+    cardsData.forEach(card => {
+        const cardHtml = `
+        <div class="wrapper1">
+            <div class="container1">
+                <div class="top" style="background-image: url(${card.imageUrl});"></div>
+                <div class="bottom">
+                    <div class="left">
+                        <div class="details">
+                            <h1 class="card1">${card.title}</h1>
+                        <p class="card2">${card.description}</p>
+                        </div>
+                        <div class="buy"><i class="fa-solid fa-arrow-right"></i></div>
+                    </div>
+                    <div class="right">
+                        <div class="done"><i class="fa-solid fa-check" style="color: black;"></i></div>
+                        <div class="details">
+
+                            <a href="${card.cardLink}">
+                                <h2>Discover more</h2>
+                                <p>${card.title} →</p>
+                            </a>
+
+                        </div>
+                        <div class="remove"><i class="fa-solid fa-xmark" style="color: red;"></i></div>
+                    </div>
+                </div>
+            </div>
+            <div class="inside">
+                <div class="icon"><i class="fa-solid fa-circle-info"></i></div>
+                <div class="contents">
+                    <ul class="ul1">
+                        <h4>Colors</h4>
+                        ${card.colors.map(color => `<li>${color}</li>`).join('')}
+                    </ul>
+                    <ul class="ul1">
+                        <h4>Size</h4>
+                        ${card.sizes.map(size => `<li>${size}</li>`).join('')}
+                    </ul>
+                    <ul class="ul1">
+                        <h4>Features</h4>
+                        ${card.features.map(feature => `<li>${feature}</li>`).join('')}
+                    </ul>
+                    <ul class="ul1">
+                        <h4>Weight</h4>
+                        <li>${card.weight}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `;
+        $('.cards-container').append(cardHtml);
+    });
+
+    $('.buy').click(function () {
+        $(this).closest('.bottom').addClass("clicked");
+    });
+
+    $('.remove').click(function () {
+        $(this).closest('.bottom').removeClass("clicked");
+    });
+});
